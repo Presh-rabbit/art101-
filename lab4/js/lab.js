f4d9eb5404e825f7faaf363bfafd231aea2f12f9
Presh-rabbit <prexs.g40@gmail.com>
2024-10-17 00:43:01 -0700

    p

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,38 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// pseudocoding and problem solving ( high level task)
+// an Implementation of the game 
+// Precious Garcia and Miki Foster <prngarci@ucsc.edu> 
+// October 16 2024
 
-// Constants
+// Task 1
+// everyday task
+// My best way to eat ramen 
+// 1. first take a picture 
+// 2. Then pick up both utensils. Your spoon and chopsticks
+// 3. With your chopsticks make sure to pick noodles, shitake mushrooms, and cabbage 
+// 4. Add all these into the spoon
+// 4. with the spoon add in a little bit of soup
+// 5. then eat and enjoy!
+// 6. repeat until soup is empty 
+// 7. you just completed the meal 
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Task 2
+// Connect_4 game
 
-// let's get this party started
-main();
+// 1. Print the board ebefore each turn 
+        //a. Create six by seven grid
+        //b. loop through each row and space
+
+// 2. Print red dot, yellow dot, and empty space
+// 3.  Print a line between them
+// 4. player automatically get colored dot chossen for 
+// 5. the game begins
+// 6. each player take turns to add dot to board
+    //a. 2 player game 
+    //b. must wait for opponent
+// 7. must connect 4 same colored dots to win
+// 8. winner wins game 
+// 9. if colored dots can not both do a row of four 
+// 10. then its a tie, good game
+
