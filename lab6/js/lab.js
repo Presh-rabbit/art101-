410b34e8920f22fc8706b0a08fb698bda67dd805
Presh-rabbit <prexs.g40@gmail.com>
2024-10-23 21:14:21 -0700

    yippie

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6 Arrays and Objects
+// Author: Precious Garcia
+// Date: October 23
 
 // Constants
 
@@ -19,3 +19,23 @@ function main() {
 
 // let's get this party started
 main();
+// define Variables
+myTransport = ["bus18", "bus20", "rides from friends", "walk"];
+
+// create a main object or my main ride 
+myMainRide= { 
+  make: "bus18",
+  model: "transit",
+  color: "blueyellow",
+  year: "2010",
+  age: function (){ 
+    return 2024 - this.year;
+  }
+}
+
+// output
+document.writeln("kinds of transportation I use: ", myTransport, "<br>");
+// this littel trick allows me to write an object in the document
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
