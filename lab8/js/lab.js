a4847521a9cc624628a8f647297ea39ec176548c
Presh-rabbit <prexs.g40@gmail.com>
2024-10-28 21:32:17 -0700

    u

---
+++ lab8/js/lab.js
@@ -0,0 +1,18 @@
+// index.js - Lab 8 Anon Fuctions and Callbacks
+// Author: Precious Garcia
+// Date: October 30
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
