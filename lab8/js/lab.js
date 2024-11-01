9c35d7795dd893f8ace66a4ea0f247c395b811bb
Presh-rabbit <prexs.g40@gmail.com>
2024-11-01 16:29:48 -0700

    anon and callback

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -2,17 +2,46 @@
 // Author: Precious Garcia
 // Date: October 30
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-    // some code here
-    // return results;
-  }
   
-  function main() {
-    console.log("Main function started.");
-    // the code that makes everything happen
-  }
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// create a fuction that adds an array by two
+function additiontwo(x) {
+  return (x + 2);}
+
+function isOdd(x) {
+  return (x - 3 == 0);
+}
+//test the function
+
+console.log("is 2 odd?", isOdd(2));
+console.log("is 3 odd?", isOdd(3));
+
+array = [17, 18, 19, 20, 21, 22, 23]
+console.log("My array", array);
+
+//test for odd numbers in array
+var oddcheckresult = array.map(isOdd);
+console.log("test for odd numbers in array", oddcheckresult);
+
+ //test addition be two in array
+
+var result = array.map(isOdd);
+//should return to [14, 15, 16, 17, 18, 19, 20]
+console.log("Test for odd numbers of the array:", result);
+
+var subtractResult = array.map(function(x){
+  return x - 3;
+});
+//should be returning to [19, 20, 21, 22, 23, 24, 25]
+console.log("Test by subtracting of 3", subtractResult);
+
+//test for ddition by 2 in array
+var additionResult = array.map(additiontwo);
+console.log("Test for addition by 2 of array",additionResult)
+
+
+
---
+++ lab9/js/lab.js
@@ -0,0 +1,34 @@
+// calculator from section D 
+
+
+  // rectangle 
+
+  // numbers 0-9 for input arrats, prompt,
+
+  function userInput () {
+
+let number;
+    number=window.prompt("please provide a number between 0 and 9");
+console.log(number); 
+return number;
+
+  }
+
+  let numberFirst = userInput();
+  let numberSecond = userInput();
+
+  console.log("numberFirst is",+ numberFirst);
+  console.log("numberSecond is",+ numberSecond);
+
+  result=numberFirst + numberSecond
+  $(".calculator").append(numeberFirst = " * ");
+  $(".calculator").append(numeberSecond = " * ");
+  
+
+
+ 
+  console.log(results);
+
+  // addition operation 
+
+  //output 
