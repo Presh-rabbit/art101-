edf2875613e6b97ecb2ef3c6b28b97d42a2f2283
Presh-rabbit <prexs.g40@gmail.com>
2024-11-04 16:04:12 -0800

    lab 9 done!

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,34 +1,44 @@
-// calculator from section D 
+/*
+   lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+
+   Requirements: jQuery must be loaded for this script to work. 
+   Lab 9: Libaries and jQuery
+
+   Author: Precious Garcia 
+   Date: Nov. 2024
+*/
+// add button to challenge section
+
+// Ensure that the script runs only after the document is fully loaded
+$(document).ready(function() {
+  // Add buttons to each section
+
+  // Add button to the "challenge" section
+  $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+  // Add button to the "problems" section
+  $("#problems").append("<button id='button-problems'>Make Special</button>");
+
+  // Add button to the "results" section
+  $("#results").append("<button id='button-results'>Make Special</button>");
+
+  // Add a click listener to the "challenge" button
+  $("#button-challenge").click(function() {
+      // Toggle the "special" class in the challenge section
+      $("#challenge").toggleClass("special");
+  });
+
+  // Add a click listener to the "problems" button
+  $("#button-problems").click(function() {
+      // Toggle the "special" class in the problems section
+      $("#problems").toggleClass("special");
+  });
+
+  // Add a click listener to the "results" button
+  $("#button-results").click(function() {
+      // Toggle the "special" class in the results section
+      $("#results").toggleClass("special");
+  });
+});
 
 
-  // rectangle 
-
-  // numbers 0-9 for input arrats, prompt,
-
-  function userInput () {
-
-let number;
-    number=window.prompt("please provide a number between 0 and 9");
-console.log(number); 
-return number;
-
-  }
-
-  let numberFirst = userInput();
-  let numberSecond = userInput();
-
-  console.log("numberFirst is",+ numberFirst);
-  console.log("numberSecond is",+ numberSecond);
-
-  result=numberFirst + numberSecond
-  $(".calculator").append(numeberFirst = " * ");
-  $(".calculator").append(numeberSecond = " * ");
-  
-
-
- 
-  console.log(results);
-
-  // addition operation 
-
-  //output 
